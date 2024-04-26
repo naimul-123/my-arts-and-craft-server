467162d8bb9eb9ffe880c631ae70cd93bc09e39b
Naimul <naimuliu@gmail.com>
2024-04-27 01:31:41 +0600

    Database connection done

--- index.js
+++ index.js
@@ -4,6 +4,44 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT
+const username = process.env.DB_USER
+const password = process.env.DB_PASSWORD
+console.log(username, password)
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${username}:${password}@cluster0.nevhe4f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const database = client.db("craftDB");
+        const craftCollection = database.collection("craftItems");
+
+        app.post('/crafts', async (req, res) => {
+            const craft = req.body;
+            console.log(craft)
+            const result = await craftCollection.insertOne(craft);
+            res.send(result)
+        })
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 app.use(cors());
 app.use(express.json());
